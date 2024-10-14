d5eac8e9030d3628d44aa1623bf4edf04183903e
antoineB <antoine597@gmail.com>
2024-10-14 23:35:58 +0200

    Add some missing API's functions.

--- low-level.lisp
+++ low-level.lisp
@@ -19,10 +19,13 @@
    #:ts-parser
    #:ts-language
    #:ts-tree
+   #:ts-query
+   #:ts-query-cursor
    #:ts-symbol
    #:ts-field-id
    #:ts-input-encoding
    #:ts-symbol-type
+   #:ts-state-id
    #:ts-log-type
    #:ts-query-predicate-step-type
    #:ts-query-error
@@ -57,39 +60,35 @@
    #:ts-parser-halt-on-error
    #:ts-tree-copy
    #:ts-tree-delete
-   #:ts-tree-root-node
    #:ts-tree-language
    #:ts-tree-edit
    #:ts-tree-get-changed-ranges
-   #:ts-node-type
-   #:ts-node-symbol
-   #:ts-node-start-byte
-   #:ts-node-start-point
-   #:ts-node-end-byte
-   #:ts-node-end-point
-   #:ts-node-string
-   #:ts-node-parent
-   #:ts-node-child
-   #:ts-node-child-count
-   #:ts-node-named-child
-   #:ts-node-named-child-count
-   #:ts-node-child-by-field-name
-   #:ts-node-child-by-field-id
-   #:ts-node-next-sibling
-   #:ts-node-prev-sibling
-   #:ts-node-next-named-sibling
-   #:ts-node-prev-named-sibling
-   #:ts-node-first-child-for-byte
-   #:ts-node-first-named-child-for-byte
-   #:ts-node-descendant-for-byte-range
-   #:ts-node-descendant-for-point-range
-   #:ts-node-named-descendant-for-byte-range
-   #:ts-node-named-descendant-for-point-range
+   #:ts-node-symbol-pointer
+   #:ts-node-start-byte-pointer
+   #:ts-node-end-byte-pointer
+   #:ts-node-string-pointer
+   #:ts-node-parent-pointer
+   #:ts-node-child-pointer
+   #:ts-node-child-count-pointer
+   #:ts-node-named-child-pointer
+   #:ts-node-named-child-count-pointer
+   #:ts-node-child-by-field-name-pointer
+   #:ts-node-child-by-field-id-pointer
+   #:ts-node-next-sibling-pointer
+   #:ts-node-prev-sibling-pointer
+   #:ts-node-next-named-sibling-pointer
+   #:ts-node-prev-named-sibling-pointer
+   #:ts-node-first-child-for-byte-pointer
+   #:ts-node-first-named-child-for-byte-pointer
+   #:ts-node-descendant-for-byte-range-pointer
+   #:ts-node-descendant-for-point-range-pointer
+   #:ts-node-named-descendant-for-byte-range-pointer
+   #:ts-node-named-descendant-for-point-range-pointer
    #:ts-node-edit
    #:ts-tree-cursor-new
    #:ts-tree-cursor-delete
    #:with-tree-cursor
-   #:ts-tree-cursor-reset
+   #:ts-tree-cursor-reset-pointer
    #:ts-tree-cursor-current-node
    #:ts-tree-cursor-current-field-name
    #:ts-tree-cursor-current-field-id
@@ -106,13 +105,64 @@
    #:ts-language-field-id-for-name
    #:ts-language-symbol-type
    #:ts-language-version
+   #:ts-language-copy
+   #:ts-language-delete
+   #:ts-language-state-count
+   #:ts-language-is-wasm
+   #:ts-language-next-state
    #:ts-tree-cursor-new-pointer
    #:ts-tree-root-node-pointer
    #:ts-node-is-named-pointer
    #:ts-tree-cursor-current-node-pointer
    #:ts-node-start-point-pointer
    #:ts-node-end-point-pointer
-   #:ts-node-type-pointer))
+   #:ts-node-type-pointer
+   #:ts-query-cursor-exec-pointer
+   #:ts-node-language-pointer
+   #:ts-node-field-name-for-child-pointer
+   #:ts-node-descendant-count-pointer
+   #:ts-node-next-parse-state-pointer
+   #:ts-node-parse-state-pointer
+   #:ts-node-has-error-pointer
+   #:ts-node-has-changes-pointer
+   #:ts-node-is-null-pointer
+   #:ts-node-is-missing-pointer
+   #:ts-node-is-extra-pointer
+   #:ts-node-is-error-pointer
+   #:ts-node-child-containing-descendant
+   #:ts-node-grammar-type
+   #:ts-node-grammar-symbol
+   #:ts-node-eq
+   #:ts-query-new
+   #:ts-query-delete
+   #:ts-query-pattern-count
+   #:ts-query-capture-count
+   #:ts-query-string-count
+   #:ts-query-start-byte-for-pattern
+   #:ts-query-end-byte-for-pattern
+   #:ts-query-predicates-for-pattern
+   #:ts-query-is-pattern-rooted
+   #:ts-query-is-pattern-non-local
+   #:ts-query-is-pattern-guaranteed-at-step
+   #:ts-query-capture-name-for-id
+   #:ts-query-capture-quantifier-for-id
+   #:ts-query-string-value-for-id
+   #:ts-query-disable-capture
+   #:ts-query-disable-pattern
+   #:ts-query-cursor-new
+   #:ts-query-cursor-delete
+   #:ts-query-cursor-did-exceed-match-limit
+   #:ts-query-cursor-match-limit
+   #:ts-query-cursor-set-match-limit
+   #:ts-query-cursor-set-byte-range
+   #:ts-query-cursor-next-match
+   #:ts-query-cursor-remove-match
+   #:ts-query-cursor-next-capture
+   #:ts-query-cursor-set-max-start-depth
+   #:ts-query-cursor-set-point-range-pointer
+   #:ts-tree-cursor-goto-first-child-for-point-pointer
+   #:ts-tree-root-node-with-offset-pointer))
+
 
 (in-package #:cl-tree-sitter/low-level)
 
@@ -139,9 +189,15 @@
 
 (defctype ts-tree :pointer)
 
+(defctype ts-query :pointer)
+
+(defctype ts-query-cursor :pointer)
+
 (defctype ts-symbol :uint16)
 
 (defctype ts-field-id :uint16)
+
+(defctype ts-state-id :uint16)
 
 ;; Enums
 
@@ -162,13 +218,6 @@
   :done
   :capture
   :string)
-
-(defcenum ts-query-error
-  :none
-  :syntax
-  :node-type
-  :field
-  :capture)
 
 ;; Structs
 
@@ -230,7 +279,7 @@
 (defcfun ts-parser-delete :void
   (parser ts-parser))
 
-(defcfun ts-parser-set-language :boolean
+(defcfun ts-parser-set-language :bool
   (parser ts-parser)
   (language ts-language))
 
@@ -288,17 +337,15 @@
   (parser ts-parser)
   (file :int))
 
+;; TODO: It doesn't exists in treesit api
 (defcfun ts-parser-halt-on-error :void
   (parser ts-parser)
-  (halt :boolean))
+  (halt :bool))
 
 (defcfun ts-tree-copy ts-tree
   (tree ts-tree))
 
 (defcfun ts-tree-delete :void
-  (tree ts-tree))
-
-(defcfun ts-tree-root-node (:struct ts-node)
   (tree ts-tree))
 
 (defcfun ts-tree-language ts-language
@@ -313,92 +360,98 @@
   (new-tree ts-tree)
   (length :pointer))
 
-(defcfun ts-node-type :string
-  (node (:struct ts-node)))
+(defcfun ts-tree-cursor-goto-first-child-for-point-pointer :int64
+  (self (:pointer (:struct ts-tree-cursor)))
+  (goal_point (:pointer (:struct ts-point))))
 
-(defcfun ts-node-symbol ts-symbol
-  (node (:struct ts-node)))
+(defcfun ts-tree-root-node-with-offset-pointer (:pointer (:struct ts-node))
+  (self ts-tree)
+  (offset_bytes :uint32)
+  (offset_extent (:pointer (:struct ts-point))))
 
-(defcfun ts-node-start-byte :uint32
-  (node (:struct ts-node)))
+(defcfun ts-node-type-pointer :string
+  (node (:pointer (:struct ts-node))))
 
-(defcfun ts-node-start-point (:struct ts-point)
-  (node (:struct ts-node)))
+(defcfun ts-node-symbol-pointer ts-symbol
+  (node (:pointer (:struct ts-node))))
 
-(defcfun ts-node-end-byte :uint32
-  (node (:struct ts-node)))
+(defcfun ts-node-start-byte-pointer :uint32
+  (node (:pointer (:struct ts-node))))
 
-(defcfun ts-node-end-point (:struct ts-point)
-  (node (:struct ts-node)))
+(defcfun ts-node-end-byte-pointer :uint32
+  (node (:pointer (:struct ts-node))))
 
-(defcfun ts-node-string :pointer
-  (node (:struct ts-node)))
+(defcfun ts-node-end-point-pointer (:pointer (:struct ts-point))
+  (node (:pointer (:struct ts-node))))
 
-(defcfun ts-node-parent (:struct ts-node)
-  (node (:struct ts-node)))
+(defcfun ts-node-string-pointer :pointer
+  (node (:pointer (:struct ts-node))))
 
-(defcfun ts-node-child (:struct ts-node)
-  (node (:struct ts-node))
+(defcfun ts-node-parent-pointer (:pointer (:struct ts-node))
+  (node (:pointer (:struct ts-node))))
+
+(defcfun ts-node-child-pointer (:pointer (:struct ts-node))
+  (node (:pointer (:struct ts-node)))
   (index :uint32))
 
-(defcfun ts-node-child-count :uint32
-  (node (:struct ts-node)))
+(defcfun ts-node-child-count-pointer :uint32
+  (node (:pointer (:struct ts-node))))
 
-(defcfun ts-node-named-child (:struct ts-node)
-  (node (:struct ts-node))
+(defcfun ts-node-named-child-pointer (:pointer (:struct ts-node))
+  (node (:pointer (:struct ts-node)))
   (index :uint32))
 
-(defcfun ts-node-named-child-count :uint32
-  (node (:struct ts-node)))
+(defcfun ts-node-named-child-count-pointer :uint32
+  (node (:pointer (:struct ts-node))))
 
-(defcfun ts-node-child-by-field-name (:struct ts-node)
-  (self (:struct ts-node))
+(defcfun ts-node-child-by-field-name-pointer (:pointer (:struct ts-node))
+  (self (:pointer (:struct ts-node)))
   (field-name :string)
   (field-name-length :uint32))
 
-(defcfun ts-node-child-by-field-id (:struct ts-node)
-  (node (:struct ts-node))
+(defcfun ts-node-child-by-field-id-pointer (:pointer (:struct ts-node))
+  (node (:pointer (:struct ts-node)))
   (id ts-field-id))
 
-(defcfun ts-node-next-sibling (:struct ts-node)
-  (node (:struct ts-node)))
+(defcfun ts-node-next-sibling-pointer (:pointer (:struct ts-node))
+  (node (:pointer (:struct ts-node))))
 
-(defcfun ts-node-prev-sibling (:struct ts-node)
-  (node (:struct ts-node)))
+(defcfun ts-node-prev-sibling-pointer (:pointer (:struct ts-node))
+  (node (:pointer (:struct ts-node))))
 
-(defcfun ts-node-next-named-sibling (:struct ts-node)
-  (node (:struct ts-node)))
+(defcfun ts-node-next-named-sibling-pointer (:pointer (:struct ts-node))
+  (node (:pointer (:struct ts-node))))
 
-(defcfun ts-node-prev-named-sibling (:struct ts-node)
-  (node (:struct ts-node)))
+(defcfun ts-node-prev-named-sibling-pointer (:pointer (:struct ts-node))
+  (node (:pointer (:struct ts-node))))
 
-(defcfun ts-node-first-child-for-byte (:struct ts-node)
-  (node (:struct ts-node))
+(defcfun ts-node-first-child-for-byte-pointer (:pointer (:struct ts-node))
+  (node (:pointer (:struct ts-node)))
   (index :uint32))
 
-(defcfun ts-node-first-named-child-for-byte (:struct ts-node)
-  (node (:struct ts-node))
+(defcfun ts-node-first-named-child-for-byte-pointer (:pointer (:struct ts-node))
+  (node (:pointer (:struct ts-node)))
   (index :uint32))
 
-(defcfun ts-node-descendant-for-byte-range (:struct ts-node)
-  (node (:struct ts-node))
+(defcfun ts-node-descendant-for-byte-range-pointer (:pointer (:struct ts-node))
+  (node (:pointer (:struct ts-node)))
   (start :uint32)
   (end :uint32))
 
-(defcfun ts-node-descendant-for-point-range (:struct ts-node)
-  (node (:struct ts-node))
-  (start (:struct ts-point))
-  (end (:struct ts-point)))
+(defcfun ts-node-descendant-for-point-range-pointer (:pointer (:struct ts-node))
+  (node (:pointer (:struct ts-node)))
+  (start (:pointer (:struct ts-point)))
+  (end (:pointer (:struct ts-point))))
 
-(defcfun ts-node-named-descendant-for-byte-range (:struct ts-node)
-  (node (:struct ts-node))
+(defcfun ts-node-named-descendant-for-byte-range-pointer (:pointer (:struct ts-node))
+  (node (:pointer (:struct ts-node)))
   (start :uint32)
   (end :uint32))
 
-(defcfun ts-node-named-descendant-for-point-range (:struct ts-node)
-  (node (:struct ts-node))
-  (start (:struct ts-point))
-  (end (:struct ts-point)))
+(defcfun ts-node-named-descendant-for-point-range-pointer (:pointer (:struct ts-node))
+  (node (:pointer (:struct ts-node)))
+  (start (:pointer (:struct ts-point)))
+  (end (:pointer (:struct ts-point))))
 
 (defcfun ts-node-edit :void
   (node (:pointer (:struct ts-node)))
@@ -424,9 +477,9 @@
               (progn ,@forms)
            (ts-tree-cursor-delete ,var))))))
 
-(defcfun ts-tree-cursor-reset :void
+(defcfun ts-tree-cursor-reset-pointer :void
   (cursor (:pointer (:struct ts-tree-cursor)))
-  (node (:struct ts-node)))
+  (node (:pointer (:struct ts-node))))
 
 (defcfun ts-tree-cursor-current-node (:struct ts-node)
   (cursor (:pointer (:struct ts-tree-cursor))))
@@ -437,13 +490,13 @@
 (defcfun ts-tree-cursor-current-field-id ts-field-id
   (cursor (:pointer (:struct ts-tree-cursor))))
 
-(defcfun ts-tree-cursor-goto-parent :boolean
+(defcfun ts-tree-cursor-goto-parent :bool
   (cursor (:pointer (:struct ts-tree-cursor))))
 
-(defcfun ts-tree-cursor-goto-next-sibling :boolean
+(defcfun ts-tree-cursor-goto-next-sibling :bool
   (cursor (:pointer (:struct ts-tree-cursor))))
 
-(defcfun ts-tree-cursor-goto-first-child :boolean
+(defcfun ts-tree-cursor-goto-first-child :bool
   (cursor (:pointer (:struct ts-tree-cursor))))
 
 (defcfun ts-tree-cursor-goto-first-child-for-byte :int64
@@ -452,7 +505,190 @@
 (defcfun ts-tree-cursor-copy (:struct ts-tree-cursor)
   (cursor (:pointer (:struct ts-tree-cursor))))
 
-;; TODO: Query section
+;; Query section
+
+(defcenum ts-query-error
+  (:none 0)
+  (:syntax 1)
+  (:node-type 2)
+  (:field 3)
+  (:capture 4)
+  (:structure 5)
+  (:language 6))
+
+(defcfun ts-query-new ts-query
+  (language ts-language)
+  (source :string)
+  (source_len :uint32)
+  (error_offset (:pointer :uint32))
+  (error_type (:pointer ts-query-error)))
+
+
+(defcfun ts-query-delete :void
+  (self ts-query))
+
+
+(defcfun ts-query-pattern-count :uint32
+  (self ts-query))
+
+
+(defcfun ts-query-capture-count :uint32
+  (self ts-query))
+
+
+(defcfun ts-query-string-count :uint32
+  (self ts-query))
+
+
+(defcfun ts-query-start-byte-for-pattern :uint32
+  (self ts-query)
+  (pattern_index :uint32))
+
+
+(defcfun ts-query-end-byte-for-pattern :uint32
+  (self ts-query)
+  (pattern_index :uint32))
+
+
+(defcenum ts-query-predicate-step-type
+  (:done 0)
+  (:capture 1)
+  (:string 2))
+
+
+(defcstruct (ts-query-predicate-step :size 8)
+  (type ts-query-predicate-step-type :offset 0)
+  (value_id :uint32 :offset 4))
+
+
+(defcfun ts-query-predicates-for-pattern (:pointer (:struct ts-query-predicate-step))
+  (self ts-query)
+  (pattern_index :uint32)
+  (step_count (:pointer :uint32)))
+
+
+(defcfun ts-query-is-pattern-rooted :bool
+  (self ts-query)
+  (pattern_index :uint32))
+
+
+(defcfun ts-query-is-pattern-non-local :bool
+  (self ts-query)
+  (pattern_index :uint32))
+
+
+(defcfun ts-query-is-pattern-guaranteed-at-step :bool
+  (self ts-query)
+  (byte_offset :uint32))
+
+
+(defcfun ts-query-capture-name-for-id :string
+  (self ts-query)
+  (index :uint32)
+  (length (:pointer :uint32)))
+
+(defcenum ts-quantifier
+  (:zero 0)
+  (:zero-or-one 1)
+  (:zero-or-more 2)
+  (:one 3)
+  (:one-or-more 4))
+
+(defcfun ts-query-capture-quantifier-for-id ts-quantifier
+  (self ts-query)
+  (pattern_index :uint32)
+  (capture_index :uint32))
+
+
+(defcfun ts-query-string-value-for-id :string
+  (self ts-query)
+  (index :uint32)
+  (length (:pointer :uint32)))
+
+
+(defcfun ts-query-disable-capture :void
+  (self ts-query)
+  (name :STRING)
+  (length :uint32))
+
+
+(defcfun ts-query-disable-pattern :void
+  (self ts-query)
+  (pattern_index :uint32))
+
+
+(defcfun ts-query-cursor-new ts-query-cursor)
+
+
+(defcfun ts-query-cursor-delete :void
+  (self ts-query-cursor))
+
+(defcfun ts-query-cursor-did-exceed-match-limit :bool
+  (self ts-query-cursor))
+
+
+(defcfun ts-query-cursor-match-limit :uint32
+  (self ts-query-cursor))
+
+
+(defcfun ts-query-cursor-set-match-limit :void
+  (self ts-query-cursor)
+  (limit :uint32))
+
+
+(defcfun ts-query-cursor-set-byte-range :void
+  (self ts-query-cursor)
+  (start_byte :uint32)
+  (end_byte :uint32))
+
+(defcfun ts-query-cursor-set-point-range-pointer :void
+  (self ts-query-cursor)
+  (start_point (:pointer (:struct ts-point)))
+  (end_point (:pointer (:struct ts-point))))
+
+(defcfun ts-query-cursor-next-match :bool
+  (self ts-query-cursor)
+  (match (:pointer (:struct ts-query-match))))
+
+
+(defcfun ts-query-cursor-remove-match :void
+  (self ts-query-cursor)
+  (match_id :uint32))
+
+
+(defcfun ts-query-cursor-next-capture :bool
+  (self ts-query-cursor)
+  (match (:pointer (:struct ts-query-match)))
+  (capture_index (:pointer :uint32)))
+
+
+(defcfun ts-query-cursor-set-max-start-depth :void
+  (self ts-query-cursor)
+  (max_start_depth :uint32))
+
+
+(defcfun ts-language-copy ts-language
+  (self ts-language))
+
+(defcfun ts-language-delete :void
+  (self ts-language))
+
+(defcfun ts-language-state-count :uint32
+  (self ts-language))
+
+(defcfun ts-language-is-wasm :bool
+  (self ts-language))
+
+(defcfun ts-language-next-state ts-state-id
+  (self ts-language)
+  (state ts-state-id)
+  (symbol ts-symbol))
+
+(defcfun ts-query-cursor-exec-pointer :void
+  (self ts-query-cursor)
+  (query ts-query)
+  (node (:pointer (:struct ts-node))))
+
 
 (defcfun ts-language-symbol-count :uint32
   (language ts-language))
@@ -465,7 +701,7 @@
   (language ts-language)
   (string :string)
   (length :uint32)
-  (is-named :boolean))
+  (is-named :bool))
 
 (defcfun ts-language-field-count :uint32
   (language ts-language))
@@ -494,17 +730,85 @@
 (defcfun ts-tree-cursor-new-pointer (:pointer (:struct ts-tree-cursor))
   (node (:pointer (:struct ts-node))))
 
-(defcfun ts-node-is-named-pointer :boolean
-  (node (:pointer (:struct ts-node))))
-
 (defcfun ts-tree-cursor-current-node-pointer (:pointer (:struct ts-node))
   (cursor (:pointer (:struct ts-tree-cursor))))
 
-(defcfun ts-node-start-point-pointer (:struct ts-point)
+(defcfun ts-node-start-point-pointer (:pointer (:struct ts-point))
   (node (:pointer (:struct ts-node))))
 
-(defcfun ts-node-end-point-pointer (:struct ts-point)
-  (node (:pointer (:struct ts-node))))
+(defcfun ts-node-child-containing-descendant (:pointer (:struct ts-node))
+  (self (:pointer (:struct ts-node)))
+  (descendant (:pointer (:struct ts-node))))
 
-(defcfun ts-node-type-pointer :string
-  (node (:pointer (:struct ts-node))))
+(defcfun ts-node-grammar-type :string
+  (self (:pointer (:struct ts-node))))
+
+(defcfun ts-node-grammar-symbol ts-symbol
+  (self (:pointer (:struct ts-node))))
+
+(defcfun ts-node-eq :bool
+  (self (:pointer (:struct ts-node)))
+  (other (:pointer (:struct ts-node))))
+
+(defcfun ts-node-is-null-pointer :bool
+  (self (:pointer (:struct ts-node))))
+
+(defcfun ts-node-is-named-pointer :bool
+  (self (:pointer (:struct ts-node))))
+
+(defcfun ts-node-is-missing-pointer :bool
+  (self (:pointer (:struct ts-node))))
+
+(defcfun ts-node-is-extra-pointer :bool
+  (self (:pointer (:struct ts-node))))
+
+(defcfun ts-node-is-error-pointer :bool
+  (self (:pointer (:struct ts-node))))
+
+(defcfun ts-node-has-changes-pointer :bool
+  (self (:pointer (:struct ts-node))))
+
+(defcfun ts-node-has-error-pointer :bool
+  (self (:pointer (:struct ts-node))))
+
+(defcfun ts-node-parse-state-pointer ts-state-id
+  (self (:pointer (:struct ts-node))))
+
+(defcfun ts-node-field-name-for-child-pointer :string
+  (self (:pointer (:struct ts-node)))
+  (child_index :uint32))
+
+(defcfun ts-node-next-parse-state-pointer ts-state-id
+  (self (:pointer (:struct ts-node))))
+
+(defcfun ts-node-descendant-count-pointer :uint32
+    (self (:pointer (:struct ts-node))))
+
+(defcfun ts-node-language-pointer ts-language
+  (self (:pointer (:struct ts-node))))
+
+;; TODO: not implemented
+;; ts-lookahead-iterator-current-symbol
+;; ts-lookahead-iterator-current-symbol-name
+;; ts-lookahead-iterator-delete
+;; ts-lookahead-iterator-language
+;; ts-lookahead-iterator-new
+;; ts-lookahead-iterator-next
+;; ts-lookahead-iterator-reset
+;; ts-lookahead-iterator-reset-state
+;; ts-parser-cancellation-flag
+;; ts-parser-set-wasm-store
+;; ts-parser-take-wasm-store
+;; ts-set-allocator
+;; ts-tree-cursor-current-depth
+;; ts-tree-cursor-current-descendant-index
+;; ts-tree-cursor-goto-descendant
+;; ts-tree-cursor-goto-last-child
+;; ts-tree-cursor-goto-previous-sibling
+;; ts-tree-cursor-reset-to
+;; ts-tree-included-ranges
+;; ts-tree-print-dot-graph
+;; ts-wasm-store-delete
+;; ts-wasm-store-language-count
+;; ts-wasm-store-load-language
+;; ts-wasm-store-new
--- tree-sitter-wrapper.c
+++ tree-sitter-wrapper.c
@@ -2,6 +2,16 @@
 
 #include <tree_sitter/api.h>
 
+static inline TSNode *node_ptr(TSNode node) {
+    TSNode *new_node = malloc(sizeof(TSNode));
+
+    if (new_node) {
+        *new_node = node;
+    }
+
+    return new_node;
+}
+
 TSNode *ts_tree_root_node_pointer(const TSTree *self) {
     TSNode *node = malloc(sizeof(TSNode));
 
@@ -22,6 +32,10 @@ TSTreeCursor *ts_tree_cursor_new_pointer(TSNode *node) {
     return cursor;
 }
 
+void ts_tree_cursor_reset_pointer(TSTreeCursor *self, TSNode *node) {
+  ts_tree_cursor_reset(self, *node);
+}
+
 TSNode *ts_tree_cursor_current_node_pointer(const TSTreeCursor *cursor) {
     TSNode *return_node = malloc(sizeof(TSNode));
 
@@ -32,19 +46,196 @@ TSNode *ts_tree_cursor_current_node_pointer(const TSTreeCursor *cursor) {
     return return_node;
 }
 
-int ts_node_is_named_pointer(TSNode *node) {
+void ts_query_cursor_exec_pointer(TSQueryCursor *self, const TSQuery *query, TSNode *node) {
+  ts_query_cursor_exec(self, query, *node);
+}
+
+bool ts_node_is_named_pointer(TSNode *node) {
     return ts_node_is_named(*node);
 }
 
-TSPoint ts_node_start_point_pointer(TSNode *node) {
-    return ts_node_start_point(*node);
+TSPoint *ts_node_start_point_pointer(TSNode *node) {
+    TSPoint *point = malloc(sizeof(TSPoint));
+
+    if (point) {
+        *point = ts_node_start_point(*node);
+    }
+
+    return point;
 }
 
+TSPoint* ts_node_end_point_pointer(TSNode *node) {
+    TSPoint *point = malloc(sizeof(TSPoint));
 
-TSPoint ts_node_end_point_pointer(TSNode *node) {
-    return ts_node_end_point(*node);
+    if (point) {
+        *point = ts_node_end_point(*node);
+    }
+
+    return point;
 }
 
 const char *ts_node_type_pointer(TSNode *node) {
     return ts_node_type(*node);
 }
+
+TSSymbol ts_node_symbol_pointer(TSNode *self) {
+  return ts_node_symbol(*self);
+}
+
+const TSLanguage *ts_node_language_pointer(TSNode *self) {
+  return ts_node_language(*self);
+}
+
+const char *ts_node_grammar_type_pointer(TSNode *self) {
+  return ts_node_grammar_type(*self);
+}
+
+TSSymbol ts_node_grammar_symbol_pointer(TSNode *self) {
+  return ts_node_grammar_symbol(*self);
+}
+
+uint32_t ts_node_start_byte_pointer(TSNode *self) {
+  return ts_node_start_byte(*self);
+}
+
+uint32_t ts_node_end_byte_pointer(TSNode *self) {
+  return ts_node_end_byte(*self);
+}
+
+char *ts_node_string_pointer(TSNode *self) {
+  return ts_node_string(*self);
+}
+
+bool ts_node_is_null_pointer(TSNode *self) {
+  return ts_node_is_null(*self);
+}
+
+bool ts_node_is_missing_pointer(TSNode *self) {
+  return ts_node_is_missing(*self);
+}
+
+bool ts_node_is_extra_pointer(TSNode *self) {
+  return ts_node_is_extra(*self);
+}
+
+bool ts_node_has_changes_pointer(TSNode *self) {
+  return ts_node_has_changes(*self);
+}
+
+bool ts_node_has_error_pointer(TSNode *self) {
+  return ts_node_has_error(*self);
+}
+
+bool ts_node_is_error_pointer(TSNode *self) {
+  return ts_node_is_error(*self);
+}
+
+TSStateId ts_node_parse_state_pointer(TSNode *self) {
+  return ts_node_parse_state(*self);
+}
+
+TSStateId ts_node_next_parse_state_pointer(TSNode *self) {
+  return ts_node_next_parse_state(*self);
+}
+
+TSNode *ts_node_parent_pointer(TSNode *self) {
+  return node_ptr(ts_node_parent(*self));
+}
+
+TSNode *ts_node_child_containing_descendant_pointer(TSNode *self, TSNode *descendant) {
+  return node_ptr(ts_node_child_containing_descendant(*self, *descendant));
+}
+
+TSNode *ts_node_child_pointer(TSNode *self, uint32_t child_index) {
+  return node_ptr(ts_node_child(*self, child_index));
+}
+
+const char *ts_node_field_name_for_child_pointer(TSNode *self, uint32_t child_index) {
+  return ts_node_field_name_for_child(*self, child_index);
+}
+
+uint32_t ts_node_child_count_pointer(TSNode *self) {
+  return ts_node_child_count(*self);
+}
+
+TSNode *ts_node_named_child_pointer(TSNode *self, uint32_t child_index) {
+  return node_ptr(ts_node_named_child(*self, child_index));
+}
+
+uint32_t ts_node_named_child_count_pointer(TSNode *self) {
+  return ts_node_named_child_count(*self);
+}
+
+TSNode *ts_node_child_by_field_name_pointer(TSNode *self,
+                                    const char *name,
+                                    uint32_t name_length) {
+  return node_ptr(ts_node_child_by_field_name(*self, name, name_length));
+}
+
+TSNode *ts_node_child_by_field_id_pointer(TSNode *self, TSFieldId field_id) {
+  return node_ptr(ts_node_child_by_field_id(*self, field_id));
+}
+
+TSNode *ts_node_next_sibling_pointer(TSNode *self) {
+  return node_ptr(ts_node_next_sibling(*self));
+}
+
+TSNode *ts_node_prev_sibling_pointer(TSNode *self) {
+  return node_ptr(ts_node_prev_sibling(*self));
+}
+
+
+TSNode *ts_node_next_named_sibling_pointer(TSNode *self) {
+  return node_ptr(ts_node_next_named_sibling(*self));
+}
+
+TSNode *ts_node_prev_named_sibling_pointer(TSNode *self) {
+  return node_ptr(ts_node_prev_named_sibling(*self));
+}
+
+TSNode *ts_node_first_child_for_byte_pointer(TSNode *self, uint32_t byte) {
+  return node_ptr(ts_node_first_child_for_byte(*self, byte));
+}
+
+
+TSNode *ts_node_first_named_child_for_byte_pointer(TSNode *self, uint32_t byte) {
+  return node_ptr(ts_node_first_named_child_for_byte(*self, byte));
+}
+
+uint32_t ts_node_descendant_count_pointer(TSNode *self) {
+  return ts_node_descendant_count(*self);
+}
+
+TSNode *ts_node_descendant_for_byte_range_pointer(TSNode *self, uint32_t start, uint32_t end) {
+  return node_ptr(ts_node_descendant_for_byte_range(*self, start, end));
+}
+
+TSNode *ts_node_descendant_for_point_range_pointer(TSNode *self, TSPoint *start, TSPoint *end) {
+  return node_ptr(ts_node_descendant_for_point_range(*self, *start, *end));
+}
+
+TSNode *ts_node_named_descendant_for_byte_range_pointer(TSNode *self, uint32_t start, uint32_t end) {
+  return node_ptr(ts_node_named_descendant_for_byte_range(*self, start, end));
+}
+
+TSNode *ts_node_named_descendant_for_point_range_pointer(TSNode *self, TSPoint *start, TSPoint *end) {
+  return node_ptr(ts_node_named_descendant_for_point_range(*self, *start, *end));
+}
+
+bool ts_node_eq_pointer(TSNode *self, TSNode *other) {
+  return ts_node_eq(*self, *other);
+}
+
+void ts_query_cursor_set_point_range_pointer(TSQueryCursor *self, TSPoint *start_point, TSPoint *end_point) {
+  return ts_query_cursor_set_point_range(self, *start_point, *end_point);
+}
+
+int64_t ts_tree_cursor_goto_first_child_for_point_pointer(TSTreeCursor *self, TSPoint *goal_point) {
+  return ts_tree_cursor_goto_first_child_for_point(self, *goal_point);
+}
+
+TSNode *ts_tree_root_node_with_offset_pointer(const TSTree *self, uint32_t offset_bytes, TSPoint *offset_extent) {
+  return node_ptr(ts_tree_root_node_with_offset(self, offset_bytes, *offset_extent));
+}
+
+// TODO: TSInput, TSLogger, TSQueryPredicateStep not implemented
